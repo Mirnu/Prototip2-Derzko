9c47db3f31c64391e450df3520cfc982c8f1fbd1
ZuzAser80 <dde4th@gmail.com>
2024-03-16 20:26:06 +0500

    semi-done

--- Assets/Src/Platform/Platform.cs
+++ Assets/Src/Platform/Platform.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -10,25 +11,31 @@ public class Platform : MonoBehaviour
     private Action MovePlatformAction;
     
     private float timer = 0;
+    private Vector3 target;
 
     private void OnCollisionEnter2D(Collision2D other) {
         if(LayerMask.LayerToName(other.gameObject.layer) != playerLayer) return;
+        target = transform.position + PlatformMoveData.PositionChange;
         MovePlatformAction += MovePlatform;
     }
 
     private void Update() => MovePlatformAction?.Invoke();
 
     private void MovePlatform() {
-        Debug.Log("MovePlatform");
+        if(Vector2.Distance(a: transform.position, target) <= 0.001f) { MovePlatformAction -= MovePlatform; return; }
         timer += Time.deltaTime;
-        transform.position = Vector3.Lerp(transform.position, transform.position + PlatformMoveData.PositionChange, timer/PlatformMoveData.moveTime);
+        transform.position = Vector3.Lerp(transform.position, target, timer/PlatformMoveData.moveTime);
         transform.rotation = Quaternion.Lerp(transform.rotation, PlatformMoveData.NewRotation, timer/PlatformMoveData.moveTime);
     }
-
 }
 [CreateAssetMenu(fileName = "MoveData", menuName = "Data/PlatformMoveData", order = 1)]
 public class MoveData : ScriptableObject {
+    [Tooltip("На сколько переместится платформа")]
     public Vector3 PositionChange;
+    [Tooltip("Конечное вращение платформы")]
     public Quaternion NewRotation;
+    [Tooltip("Время на перемещение платформы в милисекундах(вроде)")]
     public float moveTime;
+    [Tooltip("Ходит ли платформа 'кругами'")]
+    public bool loop = false;
 }
